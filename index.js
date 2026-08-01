db462d58812ffd14274b23095697f3698267555a
lebronsocoolol-sketch <lebronsocoolol@gmail.com>
2026-08-01 09:23:54 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
 
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Home page
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "verify.html"));
+});
+
+// Login route (OAuth2 will be added next)
+app.get("/login", (req, res) => {
+    res.send("Discord OAuth2 will be added here.");
+});
+
+// Callback route (OAuth2 will be added next)
+app.get("/callback", (req, res) => {
+    res.send("Discord callback received!");
+});
+
+app.listen(PORT, () => {
+    console.log(`PackGod Bot website running on port ${PORT}`);
+});
